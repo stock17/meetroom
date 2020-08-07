11ed9aed5cd7eaa4b35aba91364f69a70b7f45f4
yury <edenstack17@gmail.com>
2020-08-07 21:21:32 +0400

    add template for the main page

--- src/main/java/ru/yurima/meetingroom/controllers/MainController.java
+++ src/main/java/ru/yurima/meetingroom/controllers/MainController.java
@@ -1,5 +1,14 @@
 package ru.yurima.meetingroom.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String main(Model model) {
+        return "/index.html";
+    }
 }
